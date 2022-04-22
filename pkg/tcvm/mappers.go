32b2c252d99559a61124512d07f355f6c43ffda8
JObog <jobogbaimhe@gmail.com>
2022-04-21 21:53:05 -0400

    adding function to load program file into memory

---
+++ pkg/tcvm/mappers.go
@@ -0,0 +1,16 @@
+package tcvm
+
+var romanNumeralDict = map[string]uint8{
+	"r0":  0,
+	"r1":  1,
+	"r2":  2,
+	"r3":  3,
+	"r4":  4,
+	"r5":  5,
+	"r6":  6,
+	"r7":  7,
+	"pc":  8,
+	"sp":  9,
+	"ir":  10,
+	"rar": 11,
+}
--- pkg/tcvm/vm.go
+++ pkg/tcvm/vm.go
@@ -1,6 +1,31 @@
 package tcvm
 
+import (
+	"errors"
+	"math"
+	"os"
+)
+
+
 type VirtualMachine struct {
 	RegisterFile [16]uint32
-	Memory       [2147483647]uint8
+	Memory       [math.MaxInt32]uint8
+}
+
+func (vm *VirtualMachine) LoadFromFile(path string) (error) {
+	content, err := os.ReadFile(path) 
+	
+	if err != nil { 
+		return err
+	}
+
+	if len(content) > math.MaxInt32 {
+		return errors.New("Program size too big")
+	}
+
+	for i, b := range content {
+		vm.Memory[i] = b
+	}
+
+	return nil
 }
