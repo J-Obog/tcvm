b766f1bddf356a0220040f82259dabe56f0969b8
JObog <jobogbaimhe@gmail.com>
2022-04-21 22:49:36 -0400

    adding flags register mapping and refactoring variables

--- pkg/tcvm/vm.go
+++ pkg/tcvm/vm.go
@@ -7,22 +7,23 @@ import (
 )
 
 const ( // register mapping
-	R0 = 0 // RX = general purpose register
-	R1 = 1
-	R2 = 2 
-	R3 = 3 
-	PC = 4 // program counter
-	SP = 5 // stack pointer
-	RAR = 6 // return address register 
-	IR = 7 // instruction register
+	r0 = 0 // RX = general purpose register
+	r1 = 1
+	r2 = 2 
+	r3 = 3 
+	pc = 4 // program counter
+	sp = 5 // stack pointer
+	rar = 6 // return address register 
+	ir = 7 // instruction register
+	flg = 8 // flags
 )
 
-type VirtualMachine struct {
-	RegisterFile [16]uint32
-	Memory       [math.MaxInt32]uint8
+type VM struct {
+	reg [16]uint32
+	mem       [math.MaxInt32]uint8
 }
 
-func (vm *VirtualMachine) LoadFromFile(path string) (error) {
+func (vm *VM) LoadFromFile(path string) (error) {
 	content, err := os.ReadFile(path) 
 	
 	if err != nil { 
@@ -34,7 +35,7 @@ func (vm *VirtualMachine) LoadFromFile(path string) (error) {
 	}
 
 	for i, b := range content {
-		vm.Memory[i] = b
+		vm.mem[i] = b
 	}
 
 	return nil
