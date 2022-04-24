7be7c78007aaac3bcdb69f1a13a389d645abc1b5
JObog <jobogbaimhe@gmail.com>
2022-04-24 09:56:26 -0400

    updating parser

--- pkg/assembler/parser.go
+++ pkg/assembler/parser.go
@@ -1,17 +1,31 @@
+package assembler
 
-type Program struct { 
-	StartAddress uint32 // entry point of program, specified by __start__ lable in text section
-	DataStart uint32 // start of data segment
-	Size uint32 // size of entire program 
-	Data []byte //data segment
-	SymbolTable map[string]uint32 //table to map labels to addresses
+type Instruction struct {
+	Opcode    uint8
+	Type      uint8
+	Operands  []uint32
+	SymbolRef string
+}
+
+type SymbolTable map[string]struct {
+	Section uint8
+	Offset  uint32
+}
+
+type Program struct {
+	StartAddress uint32        // entry point of program, specified by __start__ lable in text section
+	DataStart    uint32        // start of data segment
+	Size         uint32        // size of entire program
+	Data         []byte        //data segment
+	Tbl          SymbolTable   //table to map labels to addresses
+	Code         []Instruction //list of instructions
 }
 
 type Parser struct {
 	lex *Lexer //Lexer struct reference
 }
 
-
-func (parser *Parser) Parse() *Program { 
-	//returns a pointer to a newly assembled program 
-}	
+func (parser *Parser) Parse() *Program {
+	//returns a pointer to a newly assembled program
+	return nil
+}
