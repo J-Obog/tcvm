ada484d968e56e5fd13021ecc1da6b7c5e57c0b0
JObog <jobogbaimhe@gmail.com>
2022-04-24 23:14:52 -0400

    adding statement structs to parser pkg

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -2,21 +2,6 @@ package parser
 
 import "github.com/J-Obog/tcvm/pkg/lexer"
 
-type Intsruction struct {
-	Type  uint8
-	Size  uint8
-	Value []byte
-}
-
-const ( // node type mapping
-	instruction = iota //
-	register
-	effective
-	label
-	immediate
-	data
-)
-
 type Parser struct {
 	tokens []*lexer.Token //list of tokens
 	ct *lexer.Token //current token
---
+++ pkg/parser/stmt.go
@@ -0,0 +1,43 @@
+package parser
+
+type Statement interface {
+	String() string
+}
+
+type Label struct {
+	Statement
+	Name string
+}
+
+type Data struct {
+	Statement
+	Size  uint8
+	Value []byte
+}
+
+const ( //operand modes
+	Register = iota
+	ERegister
+	Memory
+	Immediate
+)
+
+type Operand struct {
+	Size  uint8 //in bytes
+	Mode  uint8
+	Value string
+}
+
+type Instruction struct {
+	Statement
+	Opcode   string
+	Operands []Operand
+}
+
+func (op *Instruction) TotalSize() uint8 {
+	sum := uint8(0)
+	for _, opr := range op.Operands {
+		sum += opr.Size
+	}
+	return sum
+}
