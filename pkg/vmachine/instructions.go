3470feb8705abd309f1d843f2f5594f4af804605
JObog <jobogbaimhe@gmail.com>
2022-04-30 22:00:43 -0400

    new formats remove the need of storage interfaces

--- pkg/vmachine/instructions.go
+++ pkg/vmachine/instructions.go
@@ -1,49 +1,25 @@
 package vmachine
 
-import "reflect"
-
-func (vm *VM) getDest(dtype uint8) (dest Store, destLoc uint32) {
-	switch dtype {
-	case M_REG:
-		reg := vm.ram.Read(vm.pc, BYTE)
-		vm.pc++
-		return &vm.registers, reg
-
-	case M_EREG:
-		reg := vm.ram.Read(vm.pc, BYTE)
-		vm.pc++
-		addr := vm.registers.Read(reg, DWORD)
-		return &vm.ram, addr
-
-	case M_EMEM:
-		addr := vm.ram.Read(vm.pc, DWORD)
-		vm.pc += 4
-		return &vm.ram, addr
-	}
-
-	return nil, 0
-}
-
 func (vm *VM) getSrc(stype uint8, size uint8) uint32 {
 	switch stype {
 	case M_REG:
-		reg := vm.ram.Read(vm.pc, BYTE)
+		reg := vm.memRead(vm.pc, BYTE)
 		vm.pc++
-		return vm.registers.Read(reg, size)
+		return vm.regRead(reg, size)
 
 	case M_EREG:
-		reg := vm.ram.Read(vm.pc, BYTE)
+		reg := vm.memRead(vm.pc, BYTE)
 		vm.pc++
-		addr := vm.registers.Read(reg, DWORD)
-		return vm.ram.Read(addr, size)
+		addr := vm.regRead(reg, DWORD)
+		return vm.memRead(addr, size)
 
 	case M_EMEM:
-		addr := vm.ram.Read(vm.pc, DWORD)
+		addr := vm.memRead(vm.pc, DWORD)
 		vm.pc += 4
-		return vm.ram.Read(addr, size)
+		return vm.memRead(addr, size)
 
 	case M_IMMED:
-		v := vm.ram.Read(vm.pc, size)
+		v := vm.memRead(vm.pc, size)
 		vm.pc += uint32(size)
 		return v
 	}
@@ -51,52 +27,16 @@ func (vm *VM) getSrc(stype uint8, size uint8) uint32 {
 	return 0
 }
 
-func (vm *VM) getDSType() (dest *Store, destLoc uint32, src uint32, dataSize uint8) {
-	b := uint8(vm.ram.Read(vm.pc, BYTE))
-	vm.pc++
-	dt := (b >> 2) & 0x3
-	st := b & 0x3
-	sz := (b >> 4) & 0xF
-
-	d, dl := vm.getDest(dt)
-	s := vm.getSrc(st, sz)
-
-	return &d, dl, s, sz
-
+func (vm *VM) RSType() {
+	//register-source
 }
 
-func (vm *VM) getDType() (dest *Store, destLoc uint32, dataSize uint8) {
-	b := uint8(vm.ram.Read(vm.pc, BYTE))
-	vm.pc++
-	dt := b & 0x3
-	sz := (b >> 2) & 0xF
-
-	d, dl := vm.getDest(dt)
-
-	return &d, dl, sz
+func (vm *VM) MType() {
+	//mov
 }
 
-func (vm *VM) getSType() (src uint32, dataSize uint8) {
-	b := uint8(vm.ram.Read(vm.pc, BYTE))
-	vm.pc++
-	st := b & 0x3
-	sz := (b >> 2) & 0xF
-
-	s := vm.getSrc(st, sz)
-
-	return s, sz
-}
-
-func (vm *VM) getJType() (neg uint8, flag uint8, src uint32) {
-	b := uint8(vm.ram.Read(vm.pc, BYTE))
-	vm.pc++
-	st := b & 0x3
-	flg := (b >> 2) & 0xF
-	ng := (b >> 6) & 0x1
-
-	s := vm.getSrc(st, 0x4)
-
-	return ng, flg, s
+func (vm *VM) JType() {
+	//jmp
 }
 
 type opFn func(vm *VM)
@@ -107,9 +47,7 @@ func nop(vm *VM) {
 }
 
 func mov(vm *VM) {
-	d, dl, s, sz := vm.getDSType()
-	va := reflect.ValueOf(d).Elem()
-	va.Write()
+
 }
 
 func add(vm *VM) {
--- pkg/vmachine/vm.go
+++ pkg/vmachine/vm.go
@@ -8,10 +8,10 @@ import (
 
 type VM struct {
 	//register file
-	registers RegisterFile
+	regs [8]uint32
 	
 	//memory big endian
-	ram Memory
+	ram [MAX_MEM_SIZE]uint8
 
 	//program counter
 	pc uint32
@@ -37,13 +37,49 @@ func (vm *VM) LoadFromFile(path string) (error) {
 		return errors.New("Program size too big")
 	}
 
-	/*for i, b := range content {
-		vm.mem[i] = b
-	}*/
+	for i, b := range content {
+		vm.ram[i] = b
+	}
 
 	return nil
 }
 
+
+func (vm *VM) memRead(addr uint32, rsize uint8) uint32 {
+	ptr := addr
+	end := ptr + uint32(rsize)
+	data := uint32(0)
+
+	for ptr < end {
+		data <<= 8
+		data |= uint32(vm.ram[ptr])
+		ptr++
+	}
+
+	return data
+}
+
+func (vm *VM) memWrite(addr uint32, wsize uint8, data uint32) {
+	ptr := addr
+	end := ptr + uint32(wsize)
+	sfac := wsize - 1
+
+	for ptr < end {
+		word := (data & (255 << (8 * sfac))) >> (8 * sfac)
+		vm.ram[ptr] = uint8(word)
+		ptr++
+		sfac--
+	}
+}
+
+func (vm *VM) regRead(reg uint32, rsize uint8) uint32 {
+	return vm.regs[reg] & ((1 << (8 * rsize)) - 1)
+}
+
+func (vm *VM) regWrite(reg uint32, wsize uint8, data uint32) {
+	d := (data & ((1 << (8 * wsize)) - 1))
+	vm.regs[reg] = d
+}
 /*
 func (vm *VM) updateFlags(r uint8) {
 	sgn := (vm.reg[r] >> 31)
@@ -57,13 +93,10 @@ func (vm *VM) updateFlags(r uint8) {
 
 }*/
 
-
-
-
-
 func (vm *VM) getFlag(flag uint8) bool {
 	return (((1 << flag) & vm.flags) >> flag) == 1
 }
+
 
 func (vm *VM) Run() {
 	for {
@@ -72,7 +105,7 @@ func (vm *VM) Run() {
 		}
 
 		//fetch opcode
-		opcode := uint8(vm.ram.Read(vm.pc, BYTE))
+		opcode := vm.ram[vm.pc]
 		vm.pc++
 	
 		//execute
