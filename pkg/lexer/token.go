92c89d4a56279cf1136688106ad0c5c1d02f9a32
JObog <jobogbaimhe@gmail.com>
2022-05-11 13:19:47 -0400

    adding new tokens for cleaner and more dynamic parsing

--- pkg/lexer/token.go
+++ pkg/lexer/token.go
@@ -1,11 +1,16 @@
 package lexer
 
 type Token struct {
-	Type  int
+	Type  uint8
 	Image string
 }
 
 const ( //token type mapping
-	Identifier = iota
-	Number
+	IDENTIFIER  uint8 = 0
+	NUMBER      uint8 = 1
+	REGISTER    uint8 = 2
+	INSTRUCTION uint8 = 3
+	LABEL       uint8 = 4
+	ALLOCTYPE   uint8 = 5
+	DATA        uint8 = 6
 )
