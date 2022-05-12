e932340fa25b782b47b6691db5da587b58ccedeb
JObog <jobogbaimhe@gmail.com>
2022-05-11 20:36:37 -0400

    updating Assembler struct fields

--- pkg/asm/assembler.go
+++ pkg/asm/assembler.go
@@ -1,33 +1,23 @@
 package asm
 
 type Assembler struct {
-	size    uint32
-	symtab  map[string]uint32
-	program []Statement
+	symtab     map[string]uint32
+	codeSeg    []byte
+	dataSeg    []byte
+	dataRelocs []string
+	codeRelocs map[string]uint32
+	program    []Statement
 }
 
 func NewAssembler(p *Parser) *Assembler {
 	a := &Assembler{}
 	stmt := p.NextStatement()
-	loc := uint32(0)
 
 	for stmt != nil {
-		switch stmt := stmt.(type) {
-		case *Label:
-			if _, ok := a.symtab[stmt.Name]; ok { //checking if label has been declared already
-				panic("Redefinition of symbol")
-			}
-			a.symtab[stmt.Name] = loc
-
-		case *Data, *Instruction:
-			a.program = append(a.program, stmt)
-		}
-
-		loc += uint32(stmt.TotalSize())
+		a.program = append(a.program, stmt)
 		stmt = p.NextStatement()
 	}
 
-	a.size = loc
 	return a
 }
 
