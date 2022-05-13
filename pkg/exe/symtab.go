73f6280236df09d0fc298ad06fa799e2f36cc55d
JObog <jobogbaimhe@gmail.com>
2022-05-13 10:56:03 -0400

    adding methods for symtab

--- pkg/exe/symtab.go
+++ pkg/exe/symtab.go
@@ -1,12 +1,5 @@
 package exe
 
-type Symbol struct {
-	Label    string
-	Offset   uint32
-	IsExtern bool
-	IsData   bool
-}
-
 type Target struct {
 	Offset uint32
 	Index  uint32
@@ -14,4 +7,40 @@ type Target struct {
 
 type RelocTable []*Target
 
+type Symbol struct {
+	Label    string
+	Offset   uint32
+	IsExtern bool
+	IsData   bool
+}
+
 type SymbolTable []*Symbol
+
+func (st *SymbolTable) Get(lbl string) *Symbol {
+	for _, sym := range *st {
+		if (sym != nil) && (sym.Label == lbl) {
+			return sym
+		}
+	}
+	return nil
+}
+
+func (st *SymbolTable) Add(sym *Symbol) bool {
+	if st.Get(sym.Label) != nil {
+		return false
+	}
+
+	*st = append(*st, sym)
+	return true
+}
+
+func (st *SymbolTable) Remove(lbl string) bool {
+	sym := st.Get(lbl)
+
+	if sym == nil {
+		return false
+	}
+
+	sym = nil
+	return true
+}
