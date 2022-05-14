cbffa4ab0c5a79d6245d245a9c83a620d5a5b3c6
JObog <jobogbaimhe@gmail.com>
2022-05-14 13:33:39 -0400

    adding helpers to read and write u32 slices

--- pkg/vm/cpu.go
+++ pkg/vm/cpu.go
@@ -1,15 +1,24 @@
 package vm
 
 import (
+	"encoding/binary"
+
 	"github.com/J-Obog/tcvm/pkg/com"
 )
 
 const MAX_MEM_SIZE = (1 << 16)
 const REGFILE_SIZE = (1 << 4)
+
 const ( //status flag mapping
 	FLG_ZERO uint8 = 0
 	FLG_NEG  uint8 = 1
 	FLG_POS  uint8 = 2
+)
+
+const ( //size mappings
+	SZ_BYTE uint8 = 0 
+	SZ_WORD uint8 = 1
+	SZ_DWORD uint8 = 2
 )
 
 type Cpu struct {
@@ -39,6 +48,63 @@ type Cpu struct {
 
 	//return address register
 	RAR uint32 
+}
+
+func (c *Cpu) Run() {
+	for {
+		//fetch
+		op := c.Memory[c.PC]  
+		primaryOp := (op >> 5) & 0x7
+		c.PC++
+		
+		//decode/execute
+		switch primaryOp {
+		case com.NO_OPERATION_OP:
+			//no operation
+
+		case com.DATA_TRANSFER_OP:
+			d := (op >> 4) & 0x1
+			i := (op >> 3) & 0x1
+			s := (op >> 1) & 0x3
+			ind := op & 0x1
+			c.transferOp(d, i, s, ind)
+
+		case com.ARITHMETIC_LOGIC_OP:
+			f := (op >> 1) & 0xF
+			i := op & 0x1
+			c.aluOp(f, i)
+
+		case com.JUMP_OP:
+			cnd := (op >> 2) & 0x7 
+			i := (op >> 1) & 0x1
+			r := op & 0x1 
+			c.jumpOp(cnd, i, r)
+
+		case com.SYSCALL_OP:
+			c.sysCall(c.Registers[com.R0])
+		}
+	}
+}
+
+func stou32(sz uint8, s []byte) uint32 {
+	b := make([]byte, 4)
+	copy(b[4 - len(s):], s)
+	return binary.BigEndian.Uint32(b)
+}
+
+func u32tos(sz uint8, val uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, val)
+
+	switch sz {
+	case SZ_BYTE:
+		return b[3:]
+	case SZ_WORD:
+		return b[2:]
+	case SZ_DWORD:
+		return b
+	}
+	return nil
 }
 
 
@@ -99,41 +165,4 @@ func (c *Cpu) updateFlags(val uint32) {
 
 func (c *Cpu) getFlag(flag uint8) bool {
 	return (((1 << flag) & c.Flags) >> flag) == 1
-}
-
-
-func (c *Cpu) Run() {
-	for {
-		//fetch
-		op := c.Memory[c.PC]  
-		primaryOp := (op >> 5) & 0x7
-		c.PC++
-		
-		//decode/execute
-		switch primaryOp {
-		case com.NO_OPERATION_OP:
-			//no operation
-
-		case com.DATA_TRANSFER_OP:
-			d := (op >> 4) & 0x1
-			i := (op >> 3) & 0x1
-			s := (op >> 1) & 0x3
-			ind := op & 0x1
-			c.transferOp(d, i, s, ind)
-
-		case com.ARITHMETIC_LOGIC_OP:
-			f := (op >> 1) & 0xF
-			i := op & 0x1
-			c.aluOp(f, i)
-
-		case com.JUMP_OP:
-			cnd := (op >> 2) & 0x7 
-			i := (op >> 1) & 0x1
-			r := op & 0x1 
-			c.jumpOp(cnd, i, r)
-
-		case com.SYSCALL_OP:
-			c.sysCall(c.Registers[com.R0])
-		}
-	}
 }
