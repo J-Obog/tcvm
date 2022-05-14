e09f692412015000a9558ba162000b6391e75d21
JObog <jobogbaimhe@gmail.com>
2022-05-13 23:25:49 -0400

    adding decode method to write link file content into a program struct

--- pkg/exe/program.go
+++ pkg/exe/program.go
@@ -1,5 +1,7 @@
 package exe
 
+import "encoding/binary"
+
 type Target struct {
 	Offset      uint32
 	StrTabIndex uint32
@@ -7,8 +9,8 @@ type Target struct {
 
 type Symbol struct {
 	Offset   uint32
-	IsExtern bool
-	IsData   bool
+	StrTabIndex uint32
+	Flags byte //[EXTERN | DATA] 
 }
 
 type Header struct {
@@ -21,6 +23,9 @@ type Header struct {
 	CodeSegLen   uint32
 }
 
+const HEADER_LEN uint32 = 28
+const HEADER_START uint32 = 0
+
 type Program struct {
 	Header
 	CodeSeg []byte
@@ -30,10 +35,107 @@ type Program struct {
 	RelTab  []*Target
 }
 
-func (p *Program) Encode() {
-
+func stou32(bslice []byte, littleEndian bool) uint32 {
+	if littleEndian {
+		return binary.LittleEndian.Uint32(bslice)
+	} else {
+		return binary.BigEndian.Uint32(bslice)
+	}
 }
 
-func (p *Program) Decode() {
+func (p *Program) Decode(o []byte) {
+	p.decodeHeader(o)
+	p.decodeStrTab(o)	
+	p.decodeSymTab(o)
+	p.decodeRelTab(o)
+	p.decodeSegs(o)
+}
 
+func (p *Program) decodeHeader(o []byte) {
+	pos := HEADER_START
+	
+	p.EntryPoint = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.StartAddress = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.StrTabLen = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.SymTabLen = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.RelTabLen = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.DataSegLen = stou32(o[pos: pos + 4], true)
+	pos += 4
+	p.CodeSegLen = stou32(o[pos: pos + 4], true)	
+	pos += 4
+}
+
+func (p *Program) decodeStrTab(o []byte) {
+	pos := HEADER_LEN
+
+	for pos != p.StrTabLen {
+		var buf []byte
+		strLen := stou32(o[pos:pos + 4], true)
+		pos += 4
+		for pos < (pos+strLen) { 
+			buf = append(buf, o[pos])
+			pos += 1
+		}
+		p.StrTab = append(p.StrTab, string(buf))
+	}
+}
+
+func (p *Program) decodeSymTab (o []byte) {
+	pos := HEADER_LEN
+	pos += p.StrTabLen
+
+	for pos != p.SymTabLen {
+		sym := &Symbol{}
+		
+		offSet := stou32(o[pos: pos + 4], true)
+		sym.Offset = offSet
+		pos += 4
+
+		strIdx := stou32(o[pos: pos + 4], true)
+		sym.StrTabIndex = strIdx
+		pos += 4
+
+		sym.Flags = o[pos]
+		pos += 1
+
+		p.SymTab[p.StrTab[strIdx]] = sym
+	}
+}
+
+func (p *Program) decodeRelTab (o []byte) {
+	pos := HEADER_LEN
+	pos += p.StrTabLen  
+	pos += p.SymTabLen
+
+	for pos != p.RelTabLen {
+		target := &Target{}
+
+		offSet := stou32(o[pos: pos + 4], true)		
+		target.Offset = offSet
+		pos += 4
+
+		strIdx := stou32(o[pos: pos + 4], true)
+		target.StrTabIndex = strIdx
+		pos += 4
+
+		p.RelTab = append(p.RelTab, target)
+	}
+}
+
+
+func (p *Program) decodeSegs(o []byte) {
+	pos := HEADER_LEN
+	pos += p.StrTabLen  
+	pos += p.SymTabLen
+	pos += p.RelTabLen
+
+	p.DataSeg = o[pos : pos + p.DataSegLen]
+	pos += p.DataSegLen
+
+	p.CodeSeg = o[pos : pos + p.CodeSegLen]
 }
