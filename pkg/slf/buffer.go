c3767f6c80d3b6e2744e52dd2084d7bcb99736b1
JObog <jobogbaimhe@gmail.com>
2022-05-14 00:58:56 -0400

    fixing ReadU8 function

--- pkg/slf/buffer.go
+++ pkg/slf/buffer.go
@@ -10,9 +10,8 @@ func ReadU32(buf *bytes.Buffer) uint32 {
 	return binary.BigEndian.Uint32(data)
 }
 
-func ReadU8(buf *bytes.Buffer) uint32 {
-	data := buf.Next(4)
-	return binary.BigEndian.Uint32(data)
+func ReadU8(buf *bytes.Buffer) uint8 {
+	return buf.Next(1)[0]
 }
 
 func ReadStr(buf *bytes.Buffer, len uint32) string {
