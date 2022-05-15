90a0b55cb2482ad106cbce4766bf937044ba98f9
JObog <jobogbaimhe@gmail.com>
2022-05-14 20:26:48 -0400

    finished handleLabel method

--- pkg/asm/assembler.go
+++ pkg/asm/assembler.go
@@ -11,7 +11,7 @@ func (a *Assembler) AssembleProgram() *slf.Program {
 	stmt := a.par.NextStatement()
 
 	for stmt != nil {
-		switch stmt.(type) {
+		switch stmt := stmt.(type) {
 		case *Label:
 			a.handleLabel(stmt)
 		case *Data:
@@ -26,14 +26,31 @@ func (a *Assembler) AssembleProgram() *slf.Program {
 	return a.pgm
 }
 
-func (a *Assembler) handleData(data Statement) {
+func (a *Assembler) handleData(data *Data) {
 	//hadle any data we come across
 }
 
-func (a *Assembler) handleInstruction(op Statement) {
+func (a *Assembler) handleInstruction(op *Instruction) {
 	//handle any instruction we come across
 }
 
-func (a *Assembler) handleLabel(label Statement) {
-	//handle any label we come accross
+func (a *Assembler) handleLabel(label *Label) {
+	lbl := label.Name
+	sym := a.pgm.SymTab[lbl]  
+
+	if sym == nil {
+		a.pgm.StrTab = append(a.pgm.StrTab, lbl)
+		newSym := &slf.Symbol{}
+		newSym.StrTabIndex = uint32(len(a.pgm.StrTab) - 1)
+		newSym.Offset = uint32(len(a.pgm.CodeSeg))
+		a.pgm.SymTab[lbl] = sym
+	} else {
+		if ((sym.Flags >> slf.S_EXTERN) & 0x1) == 1 {
+				sym.Offset = uint32(len(a.pgm.CodeSeg))
+		} else {
+			panic("Redefinition of symbol")
+		}
+	}
+
 }
+
